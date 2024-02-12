756969f8e0ec95007cf46a82f141322c3a2c3aad
Ryan Voortman <ryanvoortman@live.com>
2024-02-12 12:55:08 -0700

    feat(final): finished running project

---
+++ src/main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type Block struct {
+	data         map[string]interface{}
+	hash         string
+	previoushash string
+	timestamp    time.Time
+	pow          int
+}
+
+type Blockchain struct {
+	genesisBlock Block
+	chain        []Block
+	difficulty   int
+}
+
+func (b Block) calculateHash() string {
+	data, _ := json.Marshal(b.data)
+	blockdata := b.previoushash + string(data) + b.timestamp.String() + strconv.Itoa(b.pow)
+	blockhash := sha256.Sum256([]byte(blockdata))
+	return fmt.Sprintf("%x", blockhash)
+}
+
+func (b *Block) mine(difficulty int) {
+	for !strings.HasPrefix(b.hash, strings.Repeat("0", difficulty)) {
+		b.pow++
+		b.hash = b.calculateHash()
+	}
+}
+
+func CreateBlockChain(difficulty int) Blockchain {
+	genesisBlock := Block{
+		hash:      "0",
+		timestamp: time.Now(),
+	}
+	return Blockchain{
+		genesisBlock,
+		[]Block{genesisBlock},
+		difficulty,
+	}
+}
+
+func (b *Blockchain) addBlock(from, to string, amount float64) {
+	blockData := map[string]interface{}{
+		"from":   from,
+		"to":     to,
+		"amount": amount,
+	}
+	lastBlock := b.chain[len(b.chain)-1]
+	newBlock := Block{
+		data:         blockData,
+		previoushash: lastBlock.hash,
+		timestamp:    time.Now(),
+	}
+	newBlock.mine(b.difficulty)
+	b.chain = append(b.chain, newBlock)
+}
+
+func (b Blockchain) isValid() bool {
+	for i := range b.chain[1:] {
+		previousBlock := b.chain[i]
+		currentBlock := b.chain[i+1]
+		if currentBlock.hash != currentBlock.calculateHash() || currentBlock.previoushash != previousBlock.hash {
+			return false
+		}
+
+	}
+	return true
+
+}
+
+func main() {
+	// create a new blockchain instance with a mining difficulty of 2
+	blockchain := CreateBlockChain(2)
+
+	// record blockchain transactions on the chain for Bob, Alice, and John
+	blockchain.addBlock("Alice", "Bob", 5)
+	blockchain.addBlock("John", "Bob", 2)
+
+	// check if the blockchain is valid; expecting true
+	fmt.Println(blockchain.isValid())
+}
